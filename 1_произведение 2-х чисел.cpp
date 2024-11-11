413535da2a480ff0c5d779087ac37b934ad1abff
Морозова_Надежда <nadiy.moro04@gmail.com>
2024-11-11 13:25:27 +0500

    Add files via upload
    28.10.24

---
+++ 1_произведение 2-х чисел.cpp
@@ -0,0 +1,14 @@
+//multiplication
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y = 0;
+    cout<< "Enter x value: ";
+    cin>> x;
+    cout<< "Enter y value: ";
+    cin>> y;
+    cout<< "The result of \"x*y\" is " << x*y << endl;
+    return 0;
+}
---
+++ 2_обратная последовательность.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int w, x, y, z, v = 0;
+    cout << "Input your 5 numbers:" << endl;
+    cin >> w >> x >> y >> z >> v;
+    cout << "Result: " << v << " " << z << " " << y << " " << x << " " << w << endl;
+    return 0;
+}
