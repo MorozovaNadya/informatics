0053bf2999e42a0d45314407f61a6dc6f7d129ea
Морозова_Надежда <nadiy.moro04@gmail.com>
2024-12-20 23:18:12 +0500

    Add files via upload
    Лабораторная работа №2

---
+++ Ханойская_башня.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+void han_town(int n, int st, int fin, int temp)
+{
+	if (n > 0)
+	{
+		han_town(n - 1, st, temp, fin);
+		cout << st << " => " << fin << '\n';
+		han_town(n - 1, temp, fin, st);
+	}
+}
+
+int main()
+{
+	int n, start, finish = 0;
+	setlocale(0, "rus");
+	cout << "Введите число дисков: ";
+	cin >> n;
+	han_town(n, 1, 3, 2);
+	return 0;
+}
